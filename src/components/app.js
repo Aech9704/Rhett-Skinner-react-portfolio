a6ed27e0e649811367edfa714effe9b40b64a250
Aech9704 <rjskinner1904@gmail.com>
2019-07-02 11:59:09 -0600

    hi

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,131 @@
 import React, { Component } from "react";
-import moment from "moment";
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+import axios from 'axios';
 
-import PortfolioContainer from "./portfolio/portfolio-container";
+import NavigationContainer from "./navigation/navigation-container";
+import Home from "./pages/home";
+import About from "./pages/about";
+import contact from "./pages/contact";
+import blog from "./pages/blog";
+import PortfolioDetail from "./portfolio/portfolioDetail";
+import Auth from "./pages/auth";
+import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loggedInStatus:"NOT_LOGGED_IN"
+    };
+
+    this.handleSuccessfulLogin = this.handleSuccessfulLogin.bind(this)
+    this.handleUnsuccessfulLogin = this.handleUnsuccessfulLogin.bind(this) 
+    this.handleSuccessfulLogout = this.handleSuccessfulLogout.bind(this)
+
+  }
+
+  handleSuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "LOGGED_IN"
+    })
+  }
+
+  handleUnsuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN"
+    });
+  }
+
+  handleSuccessfulLogout() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN"
+    });
+  }
+
+
+  cheackLoginStatus() {
+    return axios.get("https://api.devcamp.space/logged_in", {
+      withCredentials: true
+      })
+      .then(response => {
+        const loggedIn = response.data.logged_in;
+        const loggedInStatus = this.state.loggedInStatus;
+
+        // If loggedIn and status LOGGED_IN => return data
+        // if loggedIn  status NOT_LOOGED_IN => update state
+        // if not loggedIn Status LOGGED_IN => update state
+
+        if (loggedIn && loggedInStatus === "LOGGED_IN") {
+          return loggedIn;
+        } else if (loggedIn && loggedInStatus === "NOT_LOGGED_IN") {
+          this.setState({
+            loggedInStatus: "LOGGED_IN"
+          });
+        } else if (!loggedIn && loggedInStatus === "LOGGED_IN") {
+          this.setState({
+            loggedInStatus: "NOT_LOGGED_IN"
+          });
+        }
+      })
+      .catch(error => {
+        console.log("ERROR", error)
+      });
+  }
+
+  componentDidMount() {
+    this.cheackLoginStatus();
+  }
+
+  authorizedPages() {
+    return [
+      <Route path="/blog" component={blog} />
+    ]
+  }
+
   render() {
     return (
-      <div className="app">
-        <h1>Rhett Skinner Portfolio</h1>
-        <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
-        <PortfolioContainer />
-      </div>
+      <div className="container">
+        
+
+        <Router>
+          <div>
+            <NavigationContainer 
+            loggedInStatus={this.state.loggedInStatus}
+            handleUnsuccessfulLogout={this.handleUnsuccessfulLogout}
+            />
+
+            <h2>{this.state.loggedInStatus}</h2>
+
+            <Switch>
+              <Route exact path="/" component={Home} />
+
+
+              <Route 
+                path="/auth" 
+                render={props => (
+                  <Auth
+                    {...props}
+                    handleSuccessfulLogin={this.handleSuccessfulLogin}
+                    handleUnsuccessfulLogin={this.handleUnsuccessfulLogin}
+                  />
+                )} 
+              />
+
+
+              <Route path="/about-me" component={About} />
+              <Route path="/contact" component={contact} />
+              <Route exact path="/portfolio/:slug" component={PortfolioDetail} />
+              {this.state.loggedInStatus === "LOGGED_IN" ? (this.authorizedPages()) : null}
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+      </Router>
+    </div>
     );
   }
 }
--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import PortfolioItem from "./portfolio-item";
 
@@ -6,24 +7,66 @@ export default class PortfolioContainer extends Component {
   constructor() {
     super();
 
-    console.log("Portfolio container has rendered");
+    this.state = {
+      pageTitle: "welcome to my portfolio",
+      isLoading: false,
+      data: []
+    };
+
+    this.handleFilter = this.handleFilter.bind(this);
+    this.getPoertfolioItems =  this.getPoertfolioItems.bind(this);
   }
 
-  portfolioItems() {
-    const data = ["Quip", "Eventbright", "Ministry Safe"];
-
-    return data.map(item => {
-      return <PortfolioItem />;
+handleFilter(filter) {
+    this.setState({
+      data: this.state.data.filter(item => {
+        return item.category === filter;
+      })
     })
+}
+
+getPoertfolioItems() {
+  axios
+    .get('https://rhettskinner.devcamp.space/portfolio/portfolio_items')
+    .then(response => {
+      this.setState({
+        data: response.data.portfolio_items
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+portfolioItems() {
+  return this.state.data.map(item => {
+    return <PortfolioItem key={item.id} item={item} />;
+  });
+}
+
+  componentDidMount() {
+    this.getPoertfolioItems();
   }
 
   render() {
-    return (
-      <div>
-        <h2>Portfolio items go here updated...</h2>
+    if (this.state.isLoading) {
+      return <div>Loading...</div>
+    }
 
-        {this.PortfolioItems()}
-      </div>
+    return (
+        <div className="portfolio-items-wrapper">
+          <button className="btn" onClick={() => this.handleFilter("eCommerce")}>
+            eCommerce
+          </button>
+          <button className="btn" onClick={() => this.handleFilter("Scheduling")}>
+            Scheduling
+          </button>
+          <button className="btn" onClick={() => this.handleFilter("Enterprise")}>
+            Enterprise
+          </button>
+
+          {this.portfolioItems()}
+        </div>
     );
   }
 }
